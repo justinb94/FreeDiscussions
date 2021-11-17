65bf61aa31a25de179d2b04db220431868f8c27a
justinb94 <justin.baker_@outlook.com>
2021-11-17 10:55:42 +0100

    added main window

--- packages/FreeDiscussions.Client/MainWindow.xaml.cs
+++ packages/FreeDiscussions.Client/MainWindow.xaml.cs
@@ -1,25 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace FreeDiscussions.Client
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // import some functions for the custom window
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = this.WindowState == WindowState.Normal ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void GithubButton_Click(object sender, RoutedEventArgs e)
+        {
+            var uri = "https://github.com/justinb94/FreeDiscussions";
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = uri;
+            System.Diagnostics.Process.Start(psi);
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void WindowDrag(object sender, MouseButtonEventArgs e) // MouseDown
+        {
+            ReleaseCapture();
+            SendMessage(new WindowInteropHelper(this).Handle,
+                0xA1, (IntPtr)0x2, (IntPtr)0);
+        }
+
+        private void WindowResize(object sender, MouseButtonEventArgs e)
+        {
+            HwndSource hwndSource = PresentationSource.FromVisual((Visual)sender) as HwndSource;
+            SendMessage(hwndSource.Handle, 0x112, (IntPtr)61448, IntPtr.Zero);
         }
     }
 }
---
+++ packages/FreeDiscussions.Client/Models/TabItemModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace FreeDiscussions.Client.Models
+{
+    public class TabItemModel
+    {
+        public string HeaderText { get; set; }
+        public string HeaderImage { get; set; }
+        public Control Control { get; set; }
+    }
+}
