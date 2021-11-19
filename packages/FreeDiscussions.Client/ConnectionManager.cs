d86cb4096266e80551bdc35f60bf9f7279569018
justinb94 <justin.baker_@outlook.com>
2021-11-19 10:50:30 +0100

    added connection manager

---
+++ packages/FreeDiscussions.Client/ConnectionManager.cs
@@ -0,0 +1,48 @@
+﻿using FreeDiscussions.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Usenet.Nntp;
+
+namespace FreeDiscussions.Client
+{
+    public class ConnectionManager
+	{
+		private static List<NntpClient> _clients = new List<NntpClient>();
+
+		public static async Task<bool> CheckConnection(string host, int port, string userName, string password, bool useSSL)
+		{
+			var client = new NntpClient(new NntpConnection());
+			try
+			{
+				await client.ConnectAsync(host, port, useSSL);
+				client.Authenticate(userName, password);
+				client.Quit();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex);
+			}
+			return false;
+		}
+
+		public static async Task<NntpClient> GetClient()
+		{
+			var settings = SettingsModel.Read();
+			var login = SettingsModel.GetCredentials();
+
+			var result = new NntpClient(new NntpConnection());
+			try
+			{
+				await result.ConnectAsync(settings.Hostname, settings.Port, settings.SSL);
+				result.Authenticate(login.Username, login.Password);
+			}
+			catch { }
+
+			_clients.Add(result);
+
+			return result;
+		}
+	}
+}
