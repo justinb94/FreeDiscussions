248278e2a1d70abc79e2cb52159c7650af8e207d
justinb94 <justin.baker_@outlook.com>
2021-12-03 08:28:26 +0100

    ui update

---
+++ packages/FreeDiscussions.Client/Context.cs
@@ -0,0 +1,11 @@
+﻿using FreeDiscussions.Client.UI;
+
+namespace FreeDiscussions
+{
+    public class Context
+    {
+        public static Context Instance;
+
+        public IUIManager_ UIManager { get; set; }
+    }
+}
