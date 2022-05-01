c2921b4f89646a669b9d3da846ce98ca89843cee
justinb94 <justin.baker_@outlook.com>
2022-05-01 21:11:32 +0200

    fix

---
+++ packages/FreeDiscussions.Plugin/IDownloadController.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Usenet.Nntp.Models;
+
+namespace FreeDiscussions.Plugin
+{
+    public interface IDownloadController
+    {
+        Task<bool> IsFile(string articleId);
+        void DownloadArticle(string messageId, string newsgroup);
+    }
+
+}
+
---
+++ packages/FreeDiscussions.Plugin/IUIController.cs
@@ -0,0 +1,18 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreeDiscussions.Plugin
+{
+    public interface IUIController
+    {
+        Task OpenPanel(PanelType type, TabItemModel tab);
+        Task ClosePanel(string id);
+        void HideSidebar();
+        void ShowSidebar();
+        void UpdatePlugins();
+        void ShowSettingsPanel();
+    }
+}
